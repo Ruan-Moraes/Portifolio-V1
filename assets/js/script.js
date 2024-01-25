d1c3c820b5ecebd080be0d844267736599a15140
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-01-25 19:28:41 -0300

    update 25/01/2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,42 @@
-AOS.init()
+// Initialize AOS
+
+AOS.init();
 
 // Menu
 
-const menu = document
-    .querySelector('.header__menu')
-    .addEventListener('click', () => {
-        const showMenu = document.querySelector('.header__links')
-        showMenu.classList.toggle('menuIsActivated')
-    })
+const menu = document.querySelector(".header__menu");
+
+menu.addEventListener("click", () => {
+  animationMenu();
+  showBlurMenu();
+
+  const showMenu = document.querySelector(".header__links");
+
+  showMenu.classList.toggle("menuIsActivated");
+});
+
+function animationMenu() {
+  const lineOne = document.querySelector(".header__lineOne");
+  const lineTwo = document.querySelector(".header__lineTwo");
+  const lineThree = document.querySelector(".header__lineThree");
+
+  lineOne.classList.toggle("lineOneIsActivated");
+  lineTwo.classList.toggle("lineTwoIsActivated");
+  lineThree.classList.toggle("lineThreeIsActivated");
+}
+
+function showBlurMenu() {
+  const fade = document.querySelector(".fadeInMain");
+
+  fade.classList.toggle("fadeIsActivated");
+}
 
 // Settings
 
-const settings = document
-    .querySelector('.settings')
-    .addEventListener('click', () => {
-        const gear = document.querySelector('.settings > .fa-gear')
-        gear.style.transform = 'rotate(360deg)'
-        openSettings(gear)
-    })
+const settings = document.querySelector(".settings");
 
-function openSettings(gear) {
-    const gears = gear
-}
+settings.addEventListener("click", () => {
+  const gear = document.querySelector(".settings > .fa-gear");
+
+  gear.style.transform = "rotate(360deg)";
+});
