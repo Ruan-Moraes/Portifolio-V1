9405cae1619755f253bc6b7e1759fc0a6e98ead4
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-01-28 19:58:58 -0300

    update 28/01/2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ AOS.init();
 
 // Menu
 
-const menu = document.querySelector(".header__menu");
+const menu = document.querySelector('.header__menu');
 
-menu.addEventListener("click", () => {
+menu.addEventListener('click', () => {
   animationMenu();
   activateMenu();
   showBlurMenu();
@@ -14,39 +14,45 @@ menu.addEventListener("click", () => {
 });
 
 function activateMenu() {
-  const showMenu = document.querySelector(".header__links");
+  const showMenu = document.querySelector('.header__links');
 
-  showMenu.classList.toggle("menuIsActivated");
+  showMenu.classList.toggle('menuIsActivated');
 }
 
 function animationMenu() {
-  ["lineOne", "lineTwo", "lineThree"].forEach((lineClass) => {
+  ['lineOne', 'lineTwo', 'lineThree'].forEach((lineClass) => {
     const line = document.querySelector(`.header__${lineClass}`);
+
     line.classList.toggle(`${lineClass}IsActivated`);
   });
 }
 
 function showBlurMenu() {
-  const blurOnMain = document.querySelector(".BlurOnMain");
-  blurOnMain.classList.toggle("BlurOnMainIsActivated");
+  const blurOnMain = document.querySelector('.BlurOnMain');
+  
+  blurOnMain.classList.toggle('BlurOnMainIsActivated');
 }
 
 function changePageScrollingState() {
-  const bodyHTML = document.querySelector("body");
+  const bodyHTML = document.querySelector('body');
   const cssProperties = getComputedStyle(bodyHTML);
-  const overflowProperty = cssProperties.getPropertyValue("overflow");
+  const overflowProperty = cssProperties.getPropertyValue('overflow-y');
 
-  overflowProperty === "visible"
-    ? (bodyHTML.style.overflow = "hidden")
-    : (bodyHTML.style.overflow = "visible");
+  overflowProperty === 'visible'
+    ? (bodyHTML.style.overflowY = 'hidden')
+    : (bodyHTML.style.overflowY = 'visible');
 }
 
 // Settings
 
-const settings = document.querySelector(".settings");
+const settings = document.querySelector('.settings');
 
-settings.addEventListener("click", () => {
-  const gear = document.querySelector(".settings > .fa-gear");
-
-  gear.style.transform = gear.style.transform ? "" : "rotate(360deg)";
+settings.addEventListener('click', () => {
+  animationGear()
 });
+
+function animationGear() {
+  const gear = document.querySelector('.settings > .fa-gear');
+  
+  gear.style.transform = 'rotate(360deg)';
+}
