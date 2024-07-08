f7d97463b2387f7215bcfab7b6f5fdc1b856a883
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-07-08 20:58:12 -0300

    MUDANDO LÓGICA DE TRADUÇÃO

--- assets/js/modules/applyingSettings.mjs
+++ assets/js/modules/applyingSettings.mjs
@@ -192,44 +192,30 @@ function changeTextToSelectedLanguage(selectedLanguage) {
 }
 
 function changeEachTextToEnglish() {
-  if (
-    window.location.href === 'http://127.0.0.1:5501/' ||
-    window.location.href === 'http://127.0.0.1:5501/index.html' ||
-    window.location.href === 'http://127.0.0.1:5501/index.html#myProjects' ||
-    window.location.href ===
-      'http://https://ruan-moraes.github.io/Portifolio-V1/' ||
-    window.location.href ===
-      'http://https://ruan-moraes.github.io/Portifolio-V1/index.html' ||
-    window.location.href ===
-      'http://https://ruan-moraes.github.io/Portifolio-V1/index.html#myProjects' ||
-    window.location.href ===
-      'http://https://ruan-moraes.github.io/Portifolio-V1/index.html#myServices'
-  ) {
+  changeHeaderText();
+  changeFooterNavigationLinksText();
+  changeFooterContactsText();
+  changeFooterCopyText();
+
+  try {
     changeTextIndex();
+  } catch (error) {
+    console.clear();
   }
 
-  if (
-    window.location.href === 'http://127.0.0.1:5501/pages/myhistory.html' ||
-    window.location.href ===
-      'http://https://ruan-moraes.github.io/Portifolio-V1/myhistory.html'
-  ) {
+  try {
     changeMyHistoryText();
+  } catch (error) {
+    console.clear();
   }
 
-  if (
-    window.location.href === 'http://127.0.0.1:5501/pages/certificates.html' ||
-    window.location.href ===
-      'https://ruan-moraes.github.io/Portifolio-V1/certificates.html'
-  ) {
+  try {
     changeCertificatesText();
+  } catch (error) {
+    console.clear();
   }
 
   function changeTextIndex() {
-    changeHeaderText();
-    changeFooterNavigationLinksText();
-    changeFooterContactsText();
-    changeFooterCopyText();
-
     changeApresentationText();
     changeAboutMeText();
     changeProjectsText();
@@ -245,11 +231,14 @@ function changeEachTextToEnglish() {
         '.apresentation > p > span:nth-child(1)'
       ).textContent = 'Looking for my first opportunity as a';
       document.querySelector(
+        '.apresentation > p > strong:nth-child(2)'
+      ).textContent = 'Intern,';
+      document.querySelector(
         '.apresentation > p > span:nth-child(3)'
       ).textContent = 'or even as an ';
       document.querySelector(
         '.apresentation > p > strong:nth-child(4)'
-      ).textContent = 'intern,';
+      ).textContent = 'full-stack developer,';
       document.querySelector(
         '.apresentation > p > span:nth-child(5)'
       ).textContent = 'my focus is on specializing in';
@@ -415,11 +404,6 @@ function changeEachTextToEnglish() {
   }
 
   function changeMyHistoryText() {
-    changeHeaderText();
-    changeFooterNavigationLinksText();
-    changeFooterContactsText();
-    changeFooterCopyText();
-
     changeTitleTextOfMyHistory();
     changeSubTitleText();
     changeContentText();
@@ -472,10 +456,6 @@ function changeEachTextToEnglish() {
   }
 
   function changeCertificatesText() {
-    changeHeaderText();
-    changeFooterNavigationLinksText();
-    changeFooterContactsText();
-    changeFooterCopyText();
     translateText('pt', 'en');
   }
 
@@ -545,7 +525,7 @@ function changeEachTextToEnglish() {
         body: JSON.stringify({ source, target, elementsText }),
       }).then((response) => response.json());
 
-      const translatedText = textTreatment(response.translated);
+      const translatedText = await textTreatment(response.translated);
 
       elements.forEach((element, index) => {
         element.textContent = translatedText[index];
@@ -557,33 +537,271 @@ function changeEachTextToEnglish() {
     }
   }
 
-  function textTreatment(text) {
-    return text.map((text) => text.replace(/\n|\t|\nof/g, ' ').trim());
+  async function textTreatment(text) {
+    return await text.map((text) => text.replace(/\n|\t|\nof/g, ' ').trim());
   }
 }
 
 function changeEachTextToPortuguese() {
-  if (
-    window.location.href ===
-    'https://ruan-moraes.github.io/Portifolio-V1/myhistory.html'
-  ) {
-    changeMyHistoryText();
-  }
+  changeHeaderText();
+  changeFooterNavigationLinksText();
+  changeFooterContactsText();
+  changeFooterCopyText();
 
-  if (
-    window.location.href ===
-    'https://ruan-moraes.github.io/Portifolio-V1/certificates.html'
-  ) {
-    changeCertificatesText();
-  }
-
-  if (
-    window.location.href === 'https://ruan-moraes.github.io/Portifolio-V1/' ||
-    window.location.href ===
-      'https://ruan-moraes.github.io/Portifolio-V1/index.html' ||
-    window.location.href ===
-      'https://ruan-moraes.github.io/Portifolio-V1/index.html#myProjects'
-  ) {
+  try {
     changeTextIndex();
+  } catch (error) {
+    console.clear();
+  }
+
+  try {
+    changeMyHistoryText();
+  } catch (error) {
+    console.clear();
+  }
+
+  try {
+    changeCertificatesText();
+  } catch (error) {
+    console.clear();
+  }
+
+  function changeTextIndex() {
+    changeApresentationText();
+    changeAboutMeText();
+    changeProjectsText();
+    changeServicesText();
+
+    function changeApresentationText() {
+      document.querySelector('.apresentation > h2 > span').textContent =
+        'Olá, eu sou um futuro';
+      document.querySelector('.apresentation > h2 > strong').textContent =
+        'Desenvolvedor Full-Stack';
+
+      document.querySelector(
+        '.apresentation > p > span:nth-child(1)'
+      ).textContent = 'Buscando minha primeira oportunidade como';
+      document.querySelector(
+        '.apresentation > p > strong:nth-child(2)'
+      ).textContent = 'estagiário,';
+      document.querySelector(
+        '.apresentation > p > span:nth-child(3)'
+      ).textContent = 'ou até mesmo como';
+      document.querySelector(
+        '.apresentation > p > strong:nth-child(4)'
+      ).textContent = 'desenvolvedor full-stack,';
+      document.querySelector(
+        '.apresentation > p > span:nth-child(5)'
+      ).textContent = 'meu foco está em me especializar em';
+      document.querySelector(
+        '.apresentation > p > strong:nth-child(6)'
+      ).textContent = 'desenvolvimento web';
+      document.querySelector(
+        '.apresentation > p > span:nth-child(7)'
+      ).textContent = 'e';
+      document.querySelector(
+        '.apresentation > p > strong:nth-child(8)'
+      ).textContent = ' mobile,';
+      document.querySelector(
+        '.apresentation > p > span:nth-child(9)'
+      ).textContent = ' além de adquirir conhecimentos em';
+      document.querySelector(
+        '.apresentation > p > strong:nth-child(10)'
+      ).textContent = 'cibersegurança.';
+
+      document.querySelector('.apresentation > a').textContent = 'Baixar CV';
+    }
+
+    function changeAboutMeText() {
+      document
+        .querySelectorAll('.aboutMe__lines > span > span')
+        .forEach((element) => {
+          element.textContent = 'Linha';
+        });
+      document.querySelector(
+        '.aboutMe__content > div:nth-child(1) > h2'
+      ).textContent = 'minhaPessoa';
+      document.querySelector(
+        '.aboutMe__content > p:nth-child(2) > span:nth-child(1)'
+      ).textContent = 'nome:';
+      document.querySelector(
+        '.aboutMe__content > p:nth-child(3) > span:nth-child(1)'
+      ).textContent = 'anoDeNascimento:';
+      document.querySelector(
+        '.aboutMe__content > p:nth-child(4) > span:nth-child(1)'
+      ).textContent = 'disponivelParaTrabalho:';
+      document.querySelector(
+        '.aboutMe__content > p:nth-child(5) > span:nth-child(1)'
+      ).textContent = 'tecnologias:';
+      document.querySelector(
+        '.aboutMe__content > p:nth-child(6) > span:nth-child(1)'
+      ).textContent = 'ferramentas:';
+    }
+
+    function changeProjectsText() {
+      document.querySelector(
+        '.main__projects > .container > .sectionTitle > h2'
+      ).textContent = 'Meus Projetos';
+
+      document.querySelector(
+        '.main__projects > .container > .projects > .projects__pagesContainer > div > p'
+      ).textContent = 'Total de projetos:';
+      setTimeout(() => {
+        document
+          .querySelectorAll(
+            '.main__projects > .container > .projects > .projects__pagination a span'
+          )
+          .forEach((element, index) => {
+            element.textContent = `Página ${index + 1}`;
+          });
+      }, 1100);
+
+      const loadingProjects = document.querySelector(
+        '.main__projects > .container > .projects > .projects__loadingProjects'
+      );
+
+      if (loadingProjects) {
+        setTimeout(() => {
+          loadingProjects.innerHTML = `<p>Carregando Projetos...</p>`;
+        }, 0);
+      }
+    }
+
+    function changeServicesText() {
+      document.querySelector(
+        '.main__services > .container > .sectionTitle > h2'
+      ).textContent = 'Meus serviços';
+
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(1) > .services__header > .services__title > h3'
+      ).textContent = 'Desenvolvimento Web';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(1) > .services__body > .services__description p'
+      ).textContent =
+        'Desenvolvimento de landing pages, sites institucionais, e-commerces e etc.';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(1) > .services__footer a'
+      ).href =
+        'https://wa.me/5531993112958?text=Ol%C3%A1%2C%20estou%20interessado%20em%20servi%C3%A7os%20de%20Desenvolvimento%20Web%20e%20gostaria%20de%20saber%20se%20voc%C3%AA%20realiza%20esse%20tipo%20de%20trabalho.%20Se%20sim%2C%20estaria%20dispon%C3%ADvel%20para%20iniciar%20e%20qual%20seria%20o%20prazo%20estimado%3F';
+
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(2) > .services__header > .services__title > h3'
+      ).textContent = 'Desenvolvimento Mobile';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(2) > .services__body > .services__description p'
+      ).textContent =
+        'Desenvolvimento de aplicativos mobile para Android e IOS utilizando Flutter.';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(2) > .services__footer a'
+      ).href =
+        'https://wa.me/5531993112958?text=Ol%C3%A1%2C%20estou%20buscando%20servi%C3%A7os%20de%20Desenvolvimento%20Mobile%20e%20gostaria%20de%20saber%20se%20voc%C3%AA%20realiza%20esse%20tipo%20de%20trabalho.%20Se%20sim%2C%20estaria%20dispon%C3%ADvel%20para%20iniciar%20e%20qual%20seria%20o%20prazo%20estimado%3F';
+
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(3) > .services__header > .services__title > h3'
+      ).textContent = 'Cibersegurança';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(3) > .services__body > .services__description p'
+      ).textContent =
+        'Análise de vulnerabilidades, testes de invasão e proteção de dados.';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(3) > .services__footer a'
+      ).href =
+        'https://wa.me/5531993112958?text=Ol%C3%A1%2C%20estou%20interessado%20em%20servi%C3%A7os%20de%20Ciberseguran%C3%A7a%20e%20gostaria%20de%20saber%20se%20voc%C3%AA%20oferece%20esse%20tipo%20de%20trabalho.%20Se%20sim%2C%20poderia%20me%20fornecer%20mais%20informa%C3%A7%C3%B5es%20sobre%20seus%20servi%C3%A7os%20e%20qual%20seria%20a%20abordagem%20que%20voc%C3%AA%20usaria%20no%20meu%20caso%3F';
+
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(4) > .services__header > .services__title > h3'
+      ).textContent = 'Desenvolvimento e Manuntenção de API';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(4) > .services__body > .services__description p'
+      ).textContent =
+        'Desenvolvimento e manutenção de APIs para integração com outros sistemas.';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(4) > .services__footer a'
+      ).href =
+        'https://wa.me/5531993112958?text=Ol%C3%A1%2C%20estou%20interessado%20em%20servi%C3%A7os%20de%20Desenvolvimento%20e%20Manuten%C3%A7%C3%A3o%20de%20API%20e%20gostaria%20de%20saber%20se%20voc%C3%AA%20oferece%20esse%20tipo%20de%20trabalho.%20Se%20sim%2C%20poderia%20me%20fornecer%20mais%20informa%C3%A7%C3%B5es%20sobre%20seus%20servi%C3%A7os%20e%20qual%20seria%20a%20abordagem%20que%20voc%C3%AA%20usaria%20no%20meu%20caso%3F';
+
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(5) > .services__header > .services__title > h3'
+      ).textContent = 'Gereciamento de Banco de Dados';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(5) > .services__body > .services__description p'
+      ).textContent =
+        'Desenvolvimento, manuntenção e otimização de Banco de Dados relacionais (SQL) e não relacionais (NoSQL).';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(5) > .services__footer a'
+      ).href =
+        'https://wa.me/5531993112958?text=Ol%C3%A1%2C%20estou%20interessado%20em%20servi%C3%A7os%20de%20Desenvolvimento%20e%20Manuten%C3%A7%C3%A3o%20de%20Bancos%20de%20Dados%20Relacionais%20e%20N%C3%A3o-Relacionais.%20Voc%C3%AA%20poderia%20me%20fornecer%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20servi%C3%A7os%20que%20voc%C3%AA%20oferece%20nesse%20%C3%A2mbito%3F';
+
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(6) > .services__header > .services__title > h3'
+      ).textContent = 'Manutenção de Sistemas';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(6) > .services__body > .services__description p'
+      ).textContent =
+        'Realização de manutenção, refatoração / otimização, migração de tecnologias e correção de erros em vários tipos de sistemas.';
+      document.querySelector(
+        '.main__services > .container > .services > .services__item:nth-child(6) > .services__footer a'
+      ).href =
+        'https://wa.me/5531993112958?text=Ol%C3%A1%2C%20estou%20interessado%20no%20servi%C3%A7o%20de%20Manuten%C3%A7%C3%A3o%20de%20Sistemas%20Web%20e%20Mobile%2C%20Corre%C3%A7%C3%A3o%20de%20Bugs%20e%20Atualiza%C3%A7%C3%B5es.%20Voc%C3%AA%20poderia%20me%20fornecer%20mais%20informa%C3%A7%C3%B5es%20sobre%20como%20voc%C3%AA%20lida%20com%20essas%20tarefas%20e%20qual%20%C3%A9%20o%20processo%20de%20trabalho%3F';
+
+      document
+        .querySelectorAll(
+          '.main__services > .container > .services > .services__item > .services__footer > a'
+        )
+        .forEach((element) => {
+          element.textContent = 'Solicitar Serviço';
+        });
+    }
+  }
+
+  function changeHeaderText() {
+    document.querySelector('.header__title > h1 > a').textContent =
+      '< Portfólio />';
+    document.querySelector(
+      '.header__navigationLinks > a:nth-child(1)'
+    ).textContent = 'História';
+    document.querySelector(
+      '.header__navigationLinks > a:nth-child(2)'
+    ).textContent = 'Certificados';
+    document.querySelector(
+      '.header__navigationLinks > a:nth-child(3)'
+    ).textContent = 'Projetos';
+    document.querySelector('.header__navigationTitle > h2').textContent =
+      'Mais Sobre Mim';
+    document.querySelector('.header__contactsTitle > h2').textContent =
+      'Meus Contatos';
+  }
+
+  function changeFooterNavigationLinksText() {
+    document.querySelector(
+      '.footer > .container > .footer__navigationLinks > .footer__title h2'
+    ).textContent = 'Links de Navegação';
+    document.querySelector(
+      '.footer > .container > .footer__navigationLinks > .footer__links > a:nth-child(1)'
+    ).textContent = 'História';
+    document.querySelector(
+      '.footer > .container > .footer__navigationLinks > .footer__links > a:nth-child(2)'
+    ).textContent = 'Certificados';
+    document.querySelector(
+      '.footer > .container > .footer__navigationLinks > .footer__links > a:nth-child(3)'
+    ).textContent = 'Projetos';
+  }
+
+  function changeFooterContactsText() {
+    document.querySelector(
+      '.footer > .container > .footer__contacts > .footer__title h2'
+    ).textContent = 'Informações de Contatos';
+    document.querySelector(
+      '.footer > .container > .footer__contacts > .footer__tel > h3'
+    ).textContent = 'Telefone';
+  }
+
+  function changeFooterCopyText() {
+    document.querySelector(
+      '.footer > .container > .footer__copy > .footer__title h2 span'
+    ).innerHTML = 'Portfólio de';
+    document.querySelector(
+      '.footer > .container > .footer__copy > div:nth-child(2) > p > span:nth-child(1)'
+    ).textContent = 'Todos os direitos reservados';
   }
 }
